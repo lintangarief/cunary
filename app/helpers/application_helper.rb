af876ab598f62b146de1368ea5f791cb3ca300e6
Adam Cooke <adam@atechmedia.com>
2013-10-26 18:42:47 +0100

    furthering the site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  def link_to(name = nil, options = {}, html_options = {}, &block)
+    if html_options[:nav_item] && @active_nav == html_options.delete(:nav_item)
+      html_options[:class] ? html_options[:class] << " active" : html_options[:class] = "active"
+    end
+    super
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 ShoppeWebsite::Application.routes.draw do
+  
+  get 'about' => 'pages#about'
+  get 'about/partners' => 'pages#partners'
+  get 'about/license' => 'pages#license'
+  get 'about/contributing' => 'pages#contributing'
+  get 'about/logos' => 'pages#logos'
+  
   root :to => 'pages#home'
 end

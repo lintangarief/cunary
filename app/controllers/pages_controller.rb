1d971bcc3b1f968a3a6ef77bc9d89b8756213046
Adam Cooke <adam@atechmedia.com>
2013-10-29 11:43:17 +0000

    code section. closes #9

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,13 @@ class PagesController < ApplicationController
     render :layout => 'home'
   end
   
+  def release_notes
+    notes_path = Rails.root.join('db', 'CHANGELOG.md')
+    if File.exist?(notes_path)
+      @notes = File.read(notes_path)
+    else
+      @notes = ''
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 ShoppeWebsite::Application.routes.draw do
   
   get 'features' => 'pages#features'
-  get 'code' => 'pages#code'
-  get 'support' => 'pages#support'
-
+  
   get 'docs' => 'documentation#index'
   get 'docs/*path' => 'documentation#page', :as => 'docs_page'
   
+  get 'code' => 'pages#code'
+  get 'code/release-notes' => 'pages#release_notes', :as => 'code_release_notes'
+  get 'code/modules' => 'pages#modules'  
+
   get 'support' => 'pages#support'
   get 'support/involvement' => 'pages#involvement'
   get 'support/commercial' => 'pages#commercial_support'
